6c4c4d4902fd7321bda0015e7deda083f0e26129
ktchung <ktchung1102@gmail.com>
2023-04-04 14:51:55 +0800

    feat: add tab to show different list

--- src/components/VideoList/index.tsx
+++ src/components/VideoList/index.tsx
@@ -1,9 +1,15 @@
-import { type FC, memo, useContext } from 'react';
+import { type FC, memo, useContext, useState, useEffect } from 'react';
+import { Stack } from '@mui/material';
+import { groupBy } from 'lodash-es';
 import { AppContext } from '../../contexts/AppContext';
 import { VideoContext } from '../../contexts/VideoContext';
 import VideoCardGrid from '../VideoCardGrid';
+import VideoListTabs, { VideoListTabEnum } from './VideoListTabs';
+import type IVideo from '../../types/IVideo';
 
 interface Props {}
+
+type VideoListRecords = Partial<Record<VideoListTabEnum, IVideo[]>>;
 
 const VideoList: FC<Props> = () => {
   const { videos, updateVideoEpNum, updateIsCompleted } = useContext(VideoContext);
@@ -12,15 +18,32 @@ const VideoList: FC<Props> = () => {
     isEditing, setEditingItem, setRemovingItem
   } = useContext(AppContext);
 
+  const [tab, setTab] = useState<VideoListTabEnum>(VideoListTabEnum.ONGOING);
+  const [videoList, setVideoList] = useState<VideoListRecords>({});
+
+  useEffect(() => {
+    const grouped: VideoListRecords = groupBy(
+      videos,
+      ({ isCompleted }) => (
+        isCompleted ? VideoListTabEnum.COMPLETED : VideoListTabEnum.ONGOING
+      )
+    );
+
+    setVideoList(grouped);
+  }, [videos]);
+
   return (
-    <VideoCardGrid
-      videos={videos}
-      isEditing={isEditing}
-      onSelectEdit={setEditingItem}
-      onRemove={setRemovingItem}
-      onUpdateEpNum={updateVideoEpNum}
-      onUpdateIsCompleted={updateIsCompleted}
-    />
+    <Stack rowGap={2}>
+      <VideoListTabs tab={tab} setTab={setTab} />
+      <VideoCardGrid
+        videos={videoList[tab]}
+        isEditing={isEditing}
+        onSelectEdit={setEditingItem}
+        onRemove={setRemovingItem}
+        onUpdateEpNum={updateVideoEpNum}
+        onUpdateIsCompleted={updateIsCompleted}
+      />
+    </Stack>
   );
 };
 
